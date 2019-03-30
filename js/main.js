cbf130d09aa89fc446c46c5ce5b90ed2f6ce451e
chjesus <chjesussao@gmail.com>
2019-03-29 23:58:17 -0400

    smoothScroll

--- js/main.js
+++ js/main.js
@@ -10,6 +10,12 @@
 
     AOS.init();
     
+    var scroll = new SmoothScroll('a[href*="#"]',{
+        speed: 1500,
+        speedAsDuration: true,
+        easing: 'easeInOutQuad'
+    });
+    
     title.addEventListener('animationend',()=>{
         cont++;
         // subTitle.classList.add('sub--title--active')
