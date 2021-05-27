f187706def197e32f5077a80d68381484039a9d1
Jesus Chacon <chjesussao@gmail.com>
2021-05-26 23:33:52 -0400

    dependencias de styled-components

---
+++ src/components/Header/styles.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const Header = styled.header`
+  padding: 100px;
+`;
---
+++ src/views/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Home: React.FC = () => {
+  const Header = styled.h1`
+    padding: 100%;
+  `;
+
+  return <h1>Hello World!!!!!</h1>;
+};
+
+export default Home;
