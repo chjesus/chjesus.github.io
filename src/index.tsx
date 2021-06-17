b14e50deafd3755f6cc6fb56011f70a95d3451bd
Jesus Chacon <chjesussao@gmail.com>
2021-06-17 01:07:01 -0400

    configurando el provider de i18next

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,29 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
+
+import global_en from './translations/en/global.json';
+import global_es from './translations/es/global.json';
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: 'en',
+  resources: {
+    es: {
+      global: global_es,
+    },
+    en: {
+      global: global_en,
+    },
+  },
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <I18nextProvider i18n={i18next}>
     <App />
-  </React.StrictMode>,
+  </I18nextProvider>,
   document.getElementById('root')
 );
 
